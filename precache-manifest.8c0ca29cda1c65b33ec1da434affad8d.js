4e2535014ba7cc94554596de3b2630d4d94dbbe1
samblake00 <samuel.blake@ucdenver.edu>
2023-07-14 11:08:31 -0600

    deploy

---
+++ precache-manifest.8c0ca29cda1c65b33ec1da434affad8d.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eef6c4e290aabbd664aed0528d47559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e2c12cf499f711d53e",
+    "url": "/static/css/main.a260ea23.chunk.css"
+  },
+  {
+    "revision": "ed48dda700afc8738b40",
+    "url": "/static/js/2.2933db88.chunk.js"
+  },
+  {
+    "revision": "47e2c12cf499f711d53e",
+    "url": "/static/js/main.e1139456.chunk.js"
+  },
+  {
+    "revision": "cc5b680bc31c65dd8d7b",
+    "url": "/static/js/runtime-main.e723c643.js"
+  },
+  {
+    "revision": "575fc9efce88790eb7c557de5eebfa08",
+    "url": "/static/media/350_Colorado_Logo.575fc9ef.png"
+  },
+  {
+    "revision": "889e49cedf00706aea5930ea36ed6869",
+    "url": "/static/media/350_colorado.889e49ce.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "c2597a60b7742b4457079226f0d858fd",
+    "url": "/static/media/Diorama.c2597a60.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "b4b8615ce8c799ad115172480809af34",
+    "url": "/static/media/Merged document.b4b8615c.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "379540eb7fc2c053b15364a9fc8d8a6f",
+    "url": "/static/media/aws_logo.379540eb.png"
+  },
+  {
+    "revision": "cad8720d113c42423f06c972d9708404",
+    "url": "/static/media/blake_dunmire_final_report.cad8720d.pdf"
+  },
+  {
+    "revision": "1c1c3d4500550ea9561dbda5fb58f036",
+    "url": "/static/media/bmi.1c1c3d45.png"
+  },
+  {
+    "revision": "31afcaa35a8afce218993977b01fc36d",
+    "url": "/static/media/code.31afcaa3.png"
+  },
+  {
+    "revision": "4747522f9a7fead33883b6d9a734c88e",
+    "url": "/static/media/comba.4747522f.png"
+  },
+  {
+    "revision": "a2cfb98dbf4980ec6e14fc7176e1c1e8",
+    "url": "/static/media/comba_logo.a2cfb98d.jpg"
+  },
+  {
+    "revision": "33a3c81a2678b5cd3541445290197ee1",
+    "url": "/static/media/cu_boulder_logo.33a3c81a.png"
+  },
+  {
+    "revision": "aa85284894df2cf40c02fcedc23333ba",
+    "url": "/static/media/cudenver_logo.aa852848.jpg"
+  },
+  {
+    "revision": "cf4145d4af362982592ff0f42d62327c",
+    "url": "/static/media/cudenver_logo_circle.cf4145d4.png"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "03adee365ab781d8ee6fc997fa994572",
+    "url": "/static/media/energystar.03adee36.png"
+  },
+  {
+    "revision": "5e3dc5e30123d0d13218c2a7b411c40c",
+    "url": "/static/media/envi_animation.5e3dc5e3.gif"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "b1618a75bf0b2c3e6edfcd86eed79b9e",
+    "url": "/static/media/favicon.b1618a75.png"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "65eb13417cfa074f00d9bdb83bdea9f8",
+    "url": "/static/media/flightmap.65eb1341.png"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "6dc70b1f2d30c6997757933d1962b527",
+    "url": "/static/media/hero.6dc70b1f.png"
+  },
+  {
+    "revision": "4ffa0d3b6ee28b00f5d33473818e858d",
+    "url": "/static/media/hex.4ffa0d3b.png"
+  },
+  {
+    "revision": "bc889292ce2d74b0b1426a05e1ababac",
+    "url": "/static/media/highway.bc889292.png"
+  },
+  {
+    "revision": "3ded25704ed2eb4b49f556ced0b1572b",
+    "url": "/static/media/image-asset (2).3ded2570.png"
+  },
+  {
+    "revision": "cc1fd0d916af304e78a19bf1dedfb428",
+    "url": "/static/media/image-asset (5).cc1fd0d9.png"
+  },
+  {
+    "revision": "2e3fbcd41147e041e71c5cd29150dd30",
+    "url": "/static/media/joyplotfinal.2e3fbcd4.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "640daffb643097d3c227e52301d25c59",
+    "url": "/static/media/k2.640daffb.png"
+  },
+  {
+    "revision": "17638879e2cba86ba46b38ec2e3e7afe",
+    "url": "/static/media/lasvegas.17638879.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "2f347c2f7b03f4d54d53d91090ac345b",
+    "url": "/static/media/maxar_logo.2f347c2f.svg"
+  },
+  {
+    "revision": "f612bed7ad7f7e7c5158ee6b85dd690f",
+    "url": "/static/media/maxar_logo.f612bed7.jpg"
+  },
+  {
+    "revision": "2d2418e1212db1d3f8208ed8ee416f76",
+    "url": "/static/media/micromobility report.2d2418e1.pdf"
+  },
+  {
+    "revision": "5d08dd2513c5bdea73f28945cef43107",
+    "url": "/static/media/micromobility.5d08dd25.png"
+  },
+  {
+    "revision": "dec9a43a5ecc75b65a4ad0da59843bea",
+    "url": "/static/media/mountains.dec9a43a.png"
+  },
+  {
+    "revision": "c297f2e0d9ef4ed2f90efb4434f151ba",
+    "url": "/static/media/panama.c297f2e0.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "9bebe68a0a87abb157b7ac77bdcdfb32",
+    "url": "/static/media/profile.9bebe68a.png"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "a479eb55fcbe8b59e4365852881771af",
+    "url": "/static/media/sit.a479eb55.png"
+  },
+  {
+    "revision": "1dd7f887f79e82d4be165b78716df779",
+    "url": "/static/media/space.1dd7f887.jpeg"
+  },
+  {
+    "revision": "3bb9ac5704bfb1d77fc612d402baf40a",
+    "url": "/static/media/space.3bb9ac57.png"
+  },
+  {
+    "revision": "35059bea4582a85baf101274f4899db6",
+    "url": "/static/media/switzerland.35059bea.png"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "41bb3c16f51184fc690d99c98d17b63d",
+    "url": "/static/media/trees.41bb3c16.png"
+  }
+]);
